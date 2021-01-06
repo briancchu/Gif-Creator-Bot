797ce976d5f01297606a4fa387e746089d36d564
Ibiyemi Abiodun <ibiyemi@intulon.com>
2021-01-06 15:45:28 -0500

    e

--- src/bot.ts
+++ src/bot.ts
@@ -8,13 +8,13 @@ export async function runBot() {
   const client = new Client();
 
   const imgurClientId = process.env['IMGUR_CLIENT_ID'];
-  const discordToken = process.env['DISCORD_TOKEN'];
+  const discordToken = process.env['DISCORD_BOT_TOKEN'];
 
   if (!imgurClientId)
     throw new Error('Imgur client ID needs to be provided in IMGUR_CLIENT_ID environment variable.');
 
   if (!discordToken)
-    throw new Error('Discord token needs to be provided in DISCORD_TOKEN environemtn variable.');
+    throw new Error('Discord token needs to be provided in DISCORD_BOT_TOKEN environemtn variable.');
 
   client.on('ready', () => {
     console.log(`Logged in as ${client.user!.tag}!`);
