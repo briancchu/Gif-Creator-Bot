98da117a706f1f44de2c6a7d0a3c048d61314315
Ibiyemi Abiodun <ibiyemi@intulon.com>
2021-01-17 15:48:57 -0500

    Remote font loading + fix counterclockwise problem

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { runDiscordBot } from './discord-bot';
 import { runTelegramBot } from './telegram-bot';
-import { loadLocalFonts } from './util/font';
+import { loadLocalFonts, loadRemoteFonts } from './util/font';
 
 async function main() {
   console.info('starting gif creator bot');
@@ -8,6 +8,11 @@ async function main() {
   console.info('loading fonts');
 
   await loadLocalFonts();
+
+  if (process.env.GOOGLE_FONTS_API_KEY)
+    await loadRemoteFonts(process.env.GOOGLE_FONTS_API_KEY);
+  else
+    console.info('google fonts api key not detected; not retrieving remote fonts');
 
   console.info('running discord and telegram clients');
 
